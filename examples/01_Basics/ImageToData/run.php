41e382138f263f620eb5b6f6b856554fc31f4365
ddebowczyk <ddebowczyk@gmail.com>
2024-05-19 20:15:37 +0200

    Example - use of OpenAI vision model to extract data from scan

---
+++ examples/01_Basics/ImageToData/run.php
@@ -0,0 +1,56 @@
+# Image to data
+
+```php
+<?php
+$loader = require 'vendor/autoload.php';
+$loader->add('Cognesy\\Instructor\\', __DIR__ . '../../src/');
+
+use Cognesy\Instructor\Instructor;
+
+class Vendor {
+    public ?string $name = '';
+    public ?string $address = '';
+    public ?string $phone = '';
+}
+
+class ReceiptItem {
+    public string $name;
+    public ?int $quantity = 1;
+    public float $price;
+}
+
+class Receipt {
+    public Vendor $vendor;
+    /** @var ReceiptItem[] */
+    public array $items = [];
+    public ?float $subtotal;
+    public ?float $tax;
+    public ?float $tip;
+    public float $total;
+}
+
+$imagePath = __DIR__ . '/receipt.png';
+// load image and convert to base64
+$imageBase64 = base64_encode(file_get_contents($imagePath));
+// add base 64 prefix
+$imageBase64 = 'data:image/png;base64,' . $imageBase64;
+
+$input = [
+    ['role' => 'user', 'content' => [
+        ['type' => 'text', 'text' => 'Extract data from attached receipt'],
+        // ['type' => 'image_url', 'image_url' => 'https://www.inogic.com/blog/wp-content/uploads/2020/09/Receipt-Processor-AI-Builder-in-Canvas-App-9.png'],
+        ['type' => 'image_url', 'image_url' => $imageBase64],
+    ]],
+];
+
+$receipt = (new Instructor)->respond(
+    messages: $input,
+    responseModel: Receipt::class,
+    //options: ['debug' => true],
+    model: 'gpt-4-vision-preview',
+    options: ['max_tokens' => 4096]
+);
+
+dump($receipt);
+?>
+```
