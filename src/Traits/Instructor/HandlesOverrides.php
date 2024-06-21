0f6b04cff6a6d8dd874306549801306b6e327aa1
ddebowczyk <ddebowczyk@gmail.com>
2024-06-21 14:43:23 +0200

    Override deserialize/validate/transform via Instructor class

--- src/Traits/Instructor/HandlesOverrides.php
+++ src/Traits/Instructor/HandlesOverrides.php
@@ -2,6 +2,8 @@
 
 namespace Cognesy\Instructor\Traits\Instructor;
 
+use Cognesy\Instructor\Deserialization\Contracts\CanDeserializeClass;
+use Cognesy\Instructor\Transformation\Contracts\CanTransformObject;
 use Cognesy\Instructor\Validation\Contracts\CanValidateObject;
 
 trait HandlesOverrides
@@ -11,8 +13,31 @@ trait HandlesOverrides
         return $this;
     }
 
+    /** @param CanValidateObject[] $validators */
     public function withValidators(array $validators) : static {
-        $this->config->override(['instructor.validators' => $validators]);
+        $this->config->override(['validators' => $validators]);
+        return $this;
+    }
+
+    public function withTransformer(array $transformer) : static {
+        $this->config->override(['transformers' => [$transformer]]);
+        return $this;
+    }
+
+    /** @param CanTransformObject[] $transformers */
+    public function withTransformers(array $transformers) : static {
+        $this->config->override(['transformers' => $transformers]);
+        return $this;
+    }
+
+    public function withDeserializer(array $deserializer) : static {
+        $this->config->override(['deserializers' => [$deserializer]]);
+        return $this;
+    }
+
+    /** @param CanDeserializeClass[] $deserializers */
+    public function withDeserializers(array $deserializers) : static {
+        $this->config->override(['deserializers' => $deserializers]);
         return $this;
     }
 }
