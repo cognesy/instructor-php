7c665e237814faf3f2336011011b4ff7d2561c38
ddebowczyk <ddebowczyk@gmail.com>
2025-06-02 12:38:56 +0200

    Fixes in examples

--- examples/A02_Advanced/CustomClientParameters/run.php
+++ examples/A02_Advanced/CustomClientParameters/run.php
@@ -30,11 +30,11 @@ class User {
     public string $name;
 }
 
-// Build fully customized HTTP client
 $events = new EventDispatcher();
 
+// Build fully customized HTTP client
+
 $httpConfig = new HttpClientConfig(
-    httpClientType: 'symfony',
     connectTimeout: 5,
     requestTimeout: 60,
     idleTimeout: -1,
@@ -43,34 +43,38 @@ $httpConfig = new HttpClientConfig(
     failOnError: true,
 );
 
+$yourClientInstance = SymfonyHttpClient::create(['http_version' => '2.0']);
+
 $customClient = (new HttpClient)
     ->withEventDispatcher($events)
-    ->withEventListener($events)
+    ->withEventListener($events) // optional - if you want to register custom event listeners
     ->withDriver(new SymfonyDriver(
         config: $httpConfig,
-        clientInstance: SymfonyHttpClient::create(['http_version' => '2.0']),
+        clientInstance: $yourClientInstance,
         events: $events,
     ));
 
 // Create instance of LLM connection preset initialized with custom parameters
+
 $llmConfig = new LLMConfig(
     apiUrl: 'https://api.deepseek.com',
     apiKey: Env::get('DEEPSEEK_API_KEY'),
     endpoint: '/chat/completions',
     model: 'deepseek-chat',
     maxTokens: 128,
-    httpClient: 'symfony',
     providerType: 'openai-compatible',
 );
 
 // Get Instructor with the default client component overridden with your own
+
 $structuredOutput = (new StructuredOutput)
     ->withEventDispatcher($events)
-    ->withEventListener($events)
+    ->withEventListener($events) // optional - if you want to register custom event listeners
     ->withLLMConfig($llmConfig)
     ->withHttpClient($customClient);
 
 // Call with custom model and execution mode
+
 $user = $structuredOutput
     ->wiretap(fn($e) => $e->print())
     ->with(
--- examples/B02_LLMAdvanced/CustomClientParameters/run.php
+++ examples/B02_LLMAdvanced/CustomClientParameters/run.php
@@ -20,23 +20,16 @@ use Cognesy\Http\HttpClient;
 use Cognesy\Polyglot\LLM\Data\LLMConfig;
 use Cognesy\Polyglot\LLM\Inference;
 use Cognesy\Utils\Env;
+use Cognesy\Utils\Events\Event;
+use Cognesy\Utils\Events\EventDispatcher;
 use Cognesy\Utils\Str;
 use Symfony\Component\HttpClient\HttpClient as SymfonyHttpClient;
 
-// Create instance of LLM client initialized with custom parameters
-$config = new LLMConfig(
-    apiUrl: 'https://api.deepseek.com',
-    apiKey: Env::get('DEEPSEEK_API_KEY'),
-    endpoint: '/chat/completions',
-    model: 'deepseek-chat',
-    maxTokens: 128,
-    httpClient: 'guzzle',
-    providerType: 'deepseek',
-);
+$events = new EventDispatcher();
 
 // Build fully customized HTTP client
+
 $httpConfig = new HttpClientConfig(
-    httpClientType: 'symfony',
     connectTimeout: 5,
     requestTimeout: 60,
     idleTimeout: -1,
@@ -44,19 +37,42 @@ $httpConfig = new HttpClientConfig(
     poolTimeout: 60,
     failOnError: true,
 );
-$driver = new SymfonyDriver(
-    config: $httpConfig,
-    clientInstance: SymfonyHttpClient::create(['http_version' => '2.0']),
+
+$yourClientInstance = SymfonyHttpClient::create(['http_version' => '2.0']);
+
+$customClient = (new HttpClient)
+    ->withEventDispatcher($events)
+    ->withEventListener($events) // optional - if you want to register custom event listeners
+    ->withDriver(new SymfonyDriver(
+        config: $httpConfig,
+        clientInstance: $yourClientInstance,
+        events: $events,
+    ));
+
+// Create instance of LLM client initialized with custom parameters
+
+$config = new LLMConfig(
+    apiUrl: 'https://api.deepseek.com',
+    apiKey: Env::get('DEEPSEEK_API_KEY'),
+    endpoint: '/chat/completions',
+    model: 'deepseek-chat',
+    maxTokens: 128,
+    providerType: 'deepseek',
 );
-$customClient = (new HttpClient)->withDriver($driver);
+
+// Call inference API with custom client and configuration
 
 $answer = (new Inference)
+    ->withEventDispatcher($events)
+    ->withEventListener($events) // optional - if you want to register custom event listeners
     ->withConfig($config)
     ->withHttpClient($customClient)
+    ->wiretap(fn(Event $e) => $e->print())
     ->with(
         messages: [['role' => 'user', 'content' => 'What is the capital of France']],
         options: ['max_tokens' => 64]
     )
+    ->withStreaming()
     ->get();
 
 echo "USER: What is capital of France\n";
