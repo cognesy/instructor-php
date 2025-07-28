f8a657208c19965b8d8cacaeee51dcf361bb49bf
ddebowczyk <ddebowczyk@gmail.com>
2025-07-29 00:25:23 +0200

    composer updates

--- packages/templates/src/Data/TemplateInfo.php
+++ packages/templates/src/Data/TemplateInfo.php
@@ -3,16 +3,10 @@
 namespace Cognesy\Template\Data;
 
 use Cognesy\Template\Config\TemplateEngineConfig;
-use Cognesy\Template\Enums\FrontMatterFormat;
-use InvalidArgumentException;
-use Webuni\FrontMatter\FrontMatter;
-use Webuni\FrontMatter\Processor\JsonProcessor;
-use Webuni\FrontMatter\Processor\TomlProcessor;
-use Webuni\FrontMatter\Processor\YamlProcessor;
+use Cognesy\Utils\FrontMatter;
 
 class TemplateInfo
 {
-    private FrontMatter $engine;
     private array $templateData;
     private string $templateContent;
 
@@ -20,14 +14,9 @@ class TemplateInfo
         string $content,
         private ?TemplateEngineConfig $config = null,
     ) {
-        $startTag = $this->config->frontMatterTags[0] ?? '---';
-        $endTag = $this->config->frontMatterTags[1] ?? '---';
-        $format = $this->config->frontMatterFormat;
-        $this->engine = $this->makeEngine($format, $startTag, $endTag);
-
-        $document = $this->engine->parse($content);
-        $this->templateData = $document->getData();
-        $this->templateContent = $document->getContent();
+        $document = FrontMatter::parse($content);
+        $this->templateData = $document->data();
+        $this->templateContent = $document->document();
     }
 
     public function field(string $name) : mixed {
@@ -64,16 +53,5 @@ class TemplateInfo
 
     public function hasSchema() : bool {
         return $this->hasField('schema');
-    }
-
-    // INTERNAL /////////////////////////////////////////////////
-
-    private function makeEngine(FrontMatterFormat $format, string $startTag, string $endTag) : FrontMatter {
-        return match($format) {
-            FrontMatterFormat::Yaml => new FrontMatter(new YamlProcessor(), $startTag, $endTag),
-            FrontMatterFormat::Json => new FrontMatter(new JsonProcessor(), $startTag, $endTag),
-            FrontMatterFormat::Toml => new FrontMatter(new TomlProcessor(), $startTag, $endTag),
-            default => throw new InvalidArgumentException("Unknown front matter format: $format->value"),
-        };
     }
 }
